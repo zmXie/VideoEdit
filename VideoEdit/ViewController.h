b6c803c9b3c357d149d2d6ed7cff1546f20651f9
CHT-Technology <cht@CHT-TechnologydeiMac.local>
2017-03-30 15:03:18 +0800

    Initial Commit

---
+++ VideoEdit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoEdit
+//
+//  Created by CHT-Technology on 2017/3/30.
+//  Copyright © 2017年 CHT-Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VideoEdit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoEdit
+//
+//  Created by CHT-Technology on 2017/3/30.
+//  Copyright © 2017年 CHT-Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
